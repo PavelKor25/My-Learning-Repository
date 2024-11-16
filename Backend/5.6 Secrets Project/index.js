bbf981bfbf4d765c8ffb79993c2ca47da3ccfc39
Pavel <remezkorob@gmail.com>
2024-11-16 18:00:25 +0200

    Completed project '5.6'

--- Backend/5.6 Secrets Project/index.js
+++ Backend/5.6 Secrets Project/index.js
@@ -11,3 +11,37 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import axios from "axios";
+
+const port = 3000;
+const app = express();
+
+const URL_API = "https://secrets-api.appbrewery.com";
+
+/* const userBearerToken = "9bf94bbf-e915-4bed-be0c-677b2e4c8737";
+const config = {
+    headers: { Authorization: "Bearer " + userBearerToken, },
+} */
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(URL_API + "/random");
+        const randSecret = response.data;
+        res.render("index.ejs", {
+            secret: randSecret.secret,
+            user: randSecret.username,
+        });
+    } catch(error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+
+})
+
+app.listen(port, () => {
+    console.log("Listening on port " + port);
+})

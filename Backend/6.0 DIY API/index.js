ab941894eac16eca29997c028f8eab3b0ed40ddd
Pavel <remezkorob@gmail.com>
2024-12-09 14:08:37 +0200

    Project '6.0 DIY API' successfully completed

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -8,20 +9,120 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+app.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  res.json(jokes[randomIndex]);
+});
+
 
 //2. GET a specific joke
+app.get("/jokes/:id", (req, res) => {
+  /* ГЛАВНАЯ ИНФА: Оказывается, URL, выводящий :id, автоматически пополняет req.params элементом id, который имеет значение на отдельном ресурсе
+    (В данном случае - Postman) */
+  const id = parseInt(req.params.id);
+  /* 1) jokes - это массив. У массива есть метод поиска элемента find.
+     2) Очень странная анонимная функция как аргумент этого метода на самом деле здесь выводит
+     булево значение. Если стрелочная функция не имеет фигурных скобок и содержит одну строку условия или операции
+     (сравнение, равенство, присваивание, сумма чисел и т д), то функция автоматически возвращает в случае сравнения
+     булево значение либо результат операции (переменную). С фигурными скобками уже необходимо явно указывать
+     возвращаемое значение.
+     3) Ну и так, для справки: find перебирает все значения массива, пока условие в аргументе не выполнится. Аргумент joke
+     это и есть перебираемое значение. */
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+});
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const typeOfJoke = req.query.type;
+  /* filter похож на forEach с той разницей, что последний выполняет функцию для каждого элемента, ничего не возвращая,
+    а фильтр создает новый массив с теми элементами, выполняющими условную функцию (см выше)*/
+  const typedJokes = jokes.filter((element) => element.jokeType === typeOfJoke);
+  res.json(typedJokes);
+})
 
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  }
+  jokes.push(newJoke);
+  console.log(jokes.at(-1));  /* Метод массива at выводит один элемент массива с конца или с начала
+                                похожий на него метод slice возвращает диапазон элементов массива */
+  res.json(newJoke);
+})
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const jokeId = parseInt(req.params.id);
+  const jokeNum = jokes.findIndex((joke) => joke.id === jokeId);
+  console.log("Old Joke: ")
+  console.log(jokes.at(jokeNum));
+
+  const newJoke = {
+    id: jokeId,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes[jokeNum] = newJoke;
+
+  console.log("New Joke: ")
+  console.log(jokes.at(jokeNum));
+  res.json(jokes[jokeNum]);
+});
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = jokes.findIndex((joke) => joke.id === id);
+  console.log("OldJoke: ");
+  console.log(jokes.at(index));
+  /* Опять же рекомендую изучить solution.js, т к там присутствует новый тип присванивания переменных через ||, где
+    первое значение присвоится, если оно определено или не равно нулю, а в остальных случаях - второе значение присваивается.
+    Ы! */
+  if(req.body.text) {
+    jokes[index].jokeText = req.body.text;
+  }
+  if(req.body.type) {
+    jokes[index].jokeType = req.body.type;
+  }
+  console.log("New joke: ");
+  console.log(jokes.at(index));
+  res.json(jokes[index]);
+});
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const jokeIndex = jokes.findIndex((joke) => joke.id === id);
+
+  if(jokeIndex > -1) {
+    jokes.splice(jokeIndex, 1);
+    console.log(jokes);
+    res.status(200).send("Joke with id " + id + " successfully deleted.");
+  } else {
+    res.status(404).send("Joke with id " + id + " doesn't exist.");
+  }
+  console.log(jokes);
+  res.send("Joke with id: " + id + " successfully deleted.");
+});
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;  /* Этот ключ будет автоматически отправлен в качестве
+                                    скрытого параметра после отправки запроса. Он должен быть идентичен masterKey */
+  if(userKey === masterKey) {
+    jokes = [];
+    /* или jokes.splice(0, jokes.length); */
+    console.log(jokes);
+    res.sendStatus(200);
+  } else {
+    res.status(404)
+    .json({ error: "You are not authorised to perform this action."});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);

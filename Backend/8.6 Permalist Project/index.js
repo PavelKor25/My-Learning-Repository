8784df613054de4dd36615c1688495ce7c264a32
Pavel <remezkorob@gmail.com>
2025-02-08 13:42:57 +0200

    Project 8.6 was completed and project 9.1 was added

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -12,22 +13,53 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+const db = new pg.Client({
+  user: "postgres",
+  password: "123456",
+  database: "permalist",
+  host: "localhost",
+  port: 5432
+});
+db.connect();
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+  items = result.rows;
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  try {
+    await db.query(
+      "INSERT INTO items (title) VALUES ($1)",
+      [item]);
+    res.redirect("/");
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  const itemId = req.body["updatedItemId"];
+  const itemTitle = req.body["updatedItemTitle"];
+  await db.query(
+    "UPDATE items SET title = $1 WHERE id = $2",
+    [itemTitle, itemId]);
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
-
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const itemId = req.body["deleteItemId"];
+  await db.query(
+    "DELETE FROM items WHERE id = $1",
+    [itemId]
+  );
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
